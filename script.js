406e1ef239f6729c15e08dcc1fb21ef7e3443939
Caroline de Sá Teixeira <caroline.desat@gmail.com>
2024-02-16 16:26:24 +0000

    fixing variables

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 'use strict';
 
-// Selects all elements
+const header = document.querySelector('header');
 const btnPlay = document.querySelector('.btn--play')
 const btnPlayAgain = document.querySelector('.btn--playagain');
-const player1Name = document.querySelector('.player1--name');
-const player2Name = document.querySelector('.player2--name');
-const player1 = document.querySelector('.player1--image');
-const player2 = document.querySelector('.player2--image');
+const btnSubmitPlayer = document.querySelector('.btn--submitplayer');
+const namePlayer1 = document.querySelector('.name--player1');
+const namePlayer2 = document.querySelector('.name--player2');
+const displayPlayer1 = document.querySelector('.display--player1');
+const displayPlayer2 = document.querySelector('.display--player2');
+const imagePlayer1 = document.querySelector('.image--player1');
+const imagePlayer2 = document.querySelector('.image--player2');
 const timerDisplay = document.querySelector('.timer');
-const modal = document.querySelector('.modal');
-const modalText = document.querySelector('.modal--text');
-
-// player1Name.textContent = prompt("Enter the first player's name name:");
-// player2Name.textContent = prompt("Enter the second player's name name:");
+const welcomeModal = document.querySelector('.welcomeModal');
+const welcomeModalText = document.querySelector('.text--welcomemodal');
+const timerModal = document.querySelector('.timer--modal');
+const resultModal = document.querySelector('.resultModal');
+const resultModalText = document.querySelector('.text--resultmodal');
 
 let time, rock, paper, scissors, random1, random2;
 
@@ -24,21 +27,39 @@ const init = function () {
     random1 = Math.trunc(Math.random() * 3) + 1;
     random2 = Math.trunc(Math.random() * 3) + 1;
 
-    player1.classList.add('hidden');
-    player2.classList.add('hidden');
-    modal.classList.add('hidden');
+    imagePlayer1.classList.add('hidden');
+    imagePlayer2.classList.add('hidden');
+    resultModal.classList.add('hidden');
+
 }
 
 init();
 
+btnSubmitPlayer.addEventListener('click', function () {
+    // const correctPlayerName = function (player) {
+    //     const lower = player.slice(1).toLowerCase();
+    //     const upper = player.slice(0, 1).toUpperCase();
+    //     const name = upper.concat(lower);
+    //     return name;
+    // }
+    displayPlayer1.textContent = namePlayer1.value;
+    displayPlayer2.textContent = namePlayer2.value;
+    welcomeModal.classList.add('hidden');
+    displayPlayer1.classList.remove('hidden');
+    displayPlayer2.classList.remove('hidden');
+    header.classList.remove('hidden');
+    resultModal.classList.add('hidden');
+})
+
+
 const play = function () {
-    modal.classList.remove('hidden');
+    resultModal.classList.remove('hidden');
     btnPlayAgain.classList.add('hidden');
 
     time--;
     if (time > 0) {
         setTimeout(play, 1000);
-        modalText.textContent = time;
+        resultModalText.textContent = time;
     } else if (time === 0) {
         result();
         btnPlayAgain.classList.remove('hidden');
@@ -47,12 +68,12 @@ const play = function () {
 
 const randomGenerator = function () {
 
-    if (random1 === 1) player1.src = rock;
-    if (random2 === 1) player2.src = rock;
-    if (random1 === 2) player1.src = paper;
-    if (random2 === 2) player2.src = paper;
-    if (random1 === 3) player1.src = scissors;
-    if (random2 === 3) player2.src = scissors;
+    if (random1 === 1) imagePlayer1.src = rock;
+    if (random2 === 1) imagePlayer2.src = rock;
+    if (random1 === 2) imagePlayer1.src = paper;
+    if (random2 === 2) imagePlayer2.src = paper;
+    if (random1 === 3) imagePlayer1.src = scissors;
+    if (random2 === 3) imagePlayer2.src = scissors;
 
 }
 
@@ -72,27 +93,28 @@ const result = function () {
     // scissors  paper      P1
 
     if (random1 === random2) {
-        player1.classList.add('hidden');
-        player2.classList.add('hidden');
-        modalText.textContent = `Both players lost 😿`
+        imagePlayer1.classList.add('hidden');
+        imagePlayer2.classList.add('hidden');
+        resultModalText.textContent = `Both players lost 😿`
     } else if (random1 === 1 && random2 === 3 || random1 === 2 && random2 === 1 || random1 === 3 && random2 === 2) {
-        player1.classList.add('hidden');
-        player2.classList.add('hidden');
-        modalText.textContent = `Player 1 wins! 😸`
+        imagePlayer1.classList.add('hidden');
+        imagePlayer2.classList.add('hidden');
+        resultModalText.textContent = `${namePlayer1.value} wins! 😸`
     } else if (random1 === 1 && random2 === 2 || random1 === 2 && random2 === 3 || random1 === 3 && random2 === 1) {
-        player1.classList.add('hidden');
-        player2.classList.add('hidden');
-        modalText.textContent = `Player 2 wins! 😸`
+        imagePlayer1.classList.add('hidden');
+        imagePlayer2.classList.add('hidden');
+        resultModalText.textContent = `${namePlayer2.value} wins! 😸`
     }
 
-    player1.classList.remove('hidden');
-    player2.classList.remove('hidden');
+    imagePlayer1.classList.remove('hidden');
+    imagePlayer2.classList.remove('hidden');
 }
 
 btnPlay.addEventListener('click', play);
 
 btnPlayAgain.addEventListener('click', function () {
     init();
+    play();
 })
 
 
